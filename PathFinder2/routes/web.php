1a6980cbe73614fb07b8258a3c72f98ca31408aa
haidarniv <711haidarhanif@gmail.com>
2023-12-10 18:13:56 +0700

    CAREER PATH DONEEEEEEEEEEEEEE

--- PathFinder2/routes/web.php
+++ PathFinder2/routes/web.php
@@ -11,14 +11,15 @@ use App\Http\Controllers\kursusController;
 //* Route::post('/tim/simpan-tim/{idPengguna}/{namePengguna}', [TimController::class, 'simpanTim'])->name('simpan-tim');
 
 
-Route::get('/', function () {
-    return view('Adminn.adminCareerPath');
-});
+Route::get('/', [AdminController::class, 'showCareerPath']); //* DEFAULT
 
 
 // ! ==============MAHASISWA===============
 // ! CAREERPATH MAHASISWA
 Route::get('/careerPath', [MahasiswaController::class, 'showCareerPath']);
+Route::get('/CareerPath/kontenMHS/{namaKarir}/{idKarir}', [MahasiswaController::class, 'kontenMHSCareerPath'])->name('kontenKarirMHS');
+
+
 
 
 // ! ==============ADMIN===============
@@ -30,7 +31,20 @@ Route::get('/createCourseFinder', [AdminController::class, 'createCourseFinder']
 Route::get('/careerPathAdmin', [AdminController::class, 'showCareerPath']); //* DEFAULT
 Route::get('/CareerPath/CreateForm', [AdminController::class, 'createCareerPath']); //form createnya saja
 // *CRUD create karir
-Route::post('/CareerPath/Create', [AdminController::class, 'createKarir'])->name('tambahKarir');
+Route::get('/CareerPath/Create', [AdminController::class, 'createKarir'])->name('tambahKarir');
+
+// *CRUD edit dan delete karir
+Route::get('/CareerPath/konten/{namaKarir}', [AdminController::class, 'kontenKarir'])->name('kontenKarir');
+// masuk konten
+Route::get('/CareerPath/konten/{namaKarir}/{idKarir}', [AdminController::class, 'kontenKarir'])->name('kontenKarir');
+// delete
+Route::get('/CareerPath/delete/{idKarir}', [AdminController::class, 'deleteKarir'])->name('deleteKarir');
+// edit
+Route::get('/CareerPath/edit/{idKarir}', [AdminController::class, 'editKarir'])->name('editKarir');
+// update
+Route::get('/CareerPath/update/{idKarir}', [AdminController::class, 'updateCareer'])->name('updateCareer');
+
+
 
 
 
